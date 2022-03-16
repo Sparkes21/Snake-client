d4c4768d213cdf006a0445adb3192ac2452f6de1
Josh Sparkes <jshsparx@gmail.com>
2022-03-16 16:17:36 -0600

    added name

---
+++ client.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  conn.on('connect', () => {
+    console.log('connected to server');
+  });
+  conn.on('connect', () => {
+    conn.write("Name: JDS");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+
+
+module.exports = connect
